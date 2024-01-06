0d58e4d72a10b20e29debbd919d11ecb4aec6fcd
SPYRIDON MPENEKIS <sdi2300132@linux06.di.uoa.gr>
2024-01-06 21:10:52 +0200

    coop

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -1,3 +1,47 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main() {
+//     int score = 0, counter = 0;
+//     char my_choice = 'C';
+
+//     putchar(my_choice);
+//     putchar('\n');
+
+//     while (counter != 1000000) {
+//         int input = getchar();
+
+//         if(input == EOF)
+//             break;
+
+//         if (input == 'C' || input == 'D') {
+//             if (input == 'C') {
+//                 if (my_choice == 'D')
+//                     score += 5;
+//                 else
+//                     score += 3;
+//             } else if (input == 'D') {
+//                 if (my_choice == 'D')
+//                     score += 1;
+//             }
+
+//             my_choice = input;
+
+//             putchar(my_choice);
+//             putchar('\n');
+
+//             counter++;
+//         } 
+//         // else
+//         //     fflush(stdin);
+//     }
+
+//     putchar(score);
+//     putchar('\n');
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,9 +53,13 @@ int main() {
     putchar('\n');
 
     while (counter != 1000000) {
-        int input = getchar();
+        int input;
 
-        if(input == EOF)
+        do {
+            input = getchar();
+        } while (input != 'C' && input != 'D' && input != EOF);
+
+        if (input == EOF)
             break;
 
         if (input == 'C' || input == 'D') {
@@ -31,13 +79,10 @@ int main() {
             putchar('\n');
 
             counter++;
-        } 
-        // else
-        //     fflush(stdin);
+        }
     }
 
-    putchar(score);
-    putchar('\n');
+    printf("%d\n", score);
 
     return 0;
 }
